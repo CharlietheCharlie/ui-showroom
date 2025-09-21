ec7685642c4bbe7d8774908f5ec98e395acc8508
Charlie Wang <st896218@gmail.com>
2025-09-21 15:26:37 +0800

    ci: fix ci problem

--- features/aboutMe/index.tsx
+++ features/aboutMe/index.tsx
@@ -3,18 +3,19 @@ export default function AboutMe() {
     <div>
       <h2>About Me</h2>
       <p>
-        Hello! I'm Shao Chun Wang, a passionate developer with a love for creating
-        intuitive and dynamic user experiences. I have a background in computer
-        science and have worked on various projects ranging from web applications to
-        mobile apps.
+        {
+          "Hello! I'm Shao Chun Wang, a passionate developer with a love for creating intuitive and dynamic user experiences. I have a background in computer science and have worked on various projects ranging from web applications to mobile apps."
+        }
       </p>
       <p>
-        My skills include React, TypeScript, CSS, and more. I'm always eager to learn
-        new technologies and improve my craft. In my free time, I enjoy hiking,
-        photography, and exploring new places.
+        {
+          "My skills include React, TypeScript, CSS, and more. I'm always eager to learn new technologies and improve my craft. In my free time, I enjoy hiking, photography, and exploring new places."
+        }
       </p>
       <p>
-        Feel free to reach out if you'd like to connect or collaborate on a project!
+        {
+          "Feel free to reach out if you'd like to connect or collaborate on a project!"
+        }
       </p>
     </div>
   );
